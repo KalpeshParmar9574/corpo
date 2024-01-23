4ec21cb959b7d4bb7d4bedfdbe4c8bec02f71c89
kp_9574 <kalpeshparmar9574@gmail.com>
2024-01-24 01:21:11 +0530

    CA-01: update header and add icon pack

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,7 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'; 
+// Import Link from 'react-router-dom' if you're using React Router
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import styles from './header.module.scss';
 
-export default function header() {
+const Header = () => {
+  const menuItems = [
+    { label: 'Home', value: '/home' },
+    { label: 'Company', value: '/', subItems: [
+      { label: 'About Us', value: '/about-us' },
+      { label: 'Career', value: '/career' },
+      { label: 'Services', value: '/services' },
+    ]},
+    { label: 'Blog', value: '/blog' },
+    { label: 'Contact Us', value: '/contact' },
+  ];
+
+  const menuRender = (items) => {
+    return (
+      <ul className={styles['c-main-header-menu']}>
+        {items.map((item, index) => (
+          <li key={index} className={styles['c-main-header-menu-item']}>
+            {item.subItems ? (
+              // Render dropdown menu if there are subItems
+              <>
+                <span>{item.label}</span>
+                <ul className={styles['c-main-header-sub-menu']}>
+                  {menuRender(item.subItems)}
+                </ul>
+              </>
+            ) : (
+              // Render regular menu item
+              <Link to={item.value}>{item.label}</Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
-    <div>header</div>
-  )
-}
+    <>
+      <div className={styles['c-sub-header-container']}>
+        <div className={styles['c-sub-header-wrapper']}>
+          <div className={styles['c-sub-header-contact-info-wrapper']}>
+            <p className={styles['c-sub-header-contact']}>45544545454</p>
+            <p className={styles['c-sub-header-email']}>info@corpos.com</p>
+          </div>
+          <div className={styles['c-sub-header-info-social-icon-wrapper']}>
+          <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+        </div>
+      </div>
+      <div className={styles['c-main-header-wrapper']}>
+        <nav>
+          {menuRender(menuItems)}
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default Header;
